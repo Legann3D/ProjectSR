33d713a6d9d639d1536b6925a2d24e48e66fe2d8
Aurora Howard <howal002@mymail.unisa.edu.au>
2024-05-08 01:36:12 +0930

    Added functionality to chase player
    Player class methods and names assumed currently.

--- core/src/com/projectsr/game/Enemy.java
+++ core/src/com/projectsr/game/Enemy.java
@@ -62,18 +62,6 @@ public abstract class Enemy {
     public void render(SpriteBatch batch) {
 
         stateTime += Gdx.graphics.getDeltaTime();
-
-        switch(this.currentState) {
-            case CHASING:
-                // TODO: Implement chasing
-                break;
-            case ATTACKING:
-                // TODO: Implement attacking
-                break;
-            default:
-                // code block
-        }
-
         currentFrame = animation.getKeyFrame(stateTime, true);
         batch.draw(currentFrame, enemyWidth, enemyHeight);
     }
@@ -82,7 +70,39 @@ public abstract class Enemy {
      * Updates the enemy's position based on its speed and the elapsed time since the last frame.
      * @param f The time in seconds since the last update.
      */
-    public void update(float f) {
+    public void update(float f, Player player) {
+
+        switch(this.currentState) {
+            case CHASING:
+                // TODO: Implement chasing
+                // Check if the player is higher or lower than the enemy
+                if (this.getPosition().y < player.getPosition().y)
+                    // Move down
+                    this.position.y += this.speed * f * 4;
+                if (this.getPosition().y > player.getPosition().y)
+                    // Move up
+                    this.position.y -= this.speed * f * 4;
+                // Check if the player is left or right of the enemy
+                if (this.getPosition().x < player.getPosition().x)
+                    // Move right
+                    this.position.x += this.speed * f * 4;
+                if (this.getPosition().x > player.getPosition().x)
+                    // Move left
+                    this.position.x -= this.speed * f * 4;
+
+                // Check if the enemy is close enough to attack
+                if (distanceFrom(player) < 200) {
+                    // Set state to attacking
+                    currentState = STATE.ATTACKING;
+                }
+                break;
+
+            case ATTACKING:
+                // TODO: Implement attacking
+                break;
+            default:
+                // code block
+        }
         position.x += speed * f;
         updateCollision();
     }
@@ -97,5 +117,15 @@ public abstract class Enemy {
      */
     public void dispose() {
         // Dispose of resources
+    }
+
+    public float distanceFrom(Player player) {
+        return this.getPosition().dst(player.getPosition()); // TODO: Need player class and methods
+    }
+
+    public Vector2 getPosition() {
+        float currentX = this.position.x + (this.currentFrame.getWidth()  / 2.0f);
+        float currentY = this.position.y + (this.currentFrame.getHeight() / 2.0f);
+        return new Vector2(currentX, currentY);
     }
 }
