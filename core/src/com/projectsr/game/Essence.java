c0f949945e771e709a34108bcafaea127dccc474
Aurora Howard <howal002@mymail.unisa.edu.au>
2024-06-01 04:53:11 +0930

    Added essences and basic logic in class
    Switch case the texture used for rendering depending.

---
+++ core/src/com/projectsr/game/Essence.java
@@ -0,0 +1,51 @@
+package com.projectsr.game;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Essence {
+
+    public enum Type {
+        GREEN,
+        RED
+    }
+
+    private Type type;
+    private AssetManager assetManager;
+    private Vector2 position;
+    private Texture texture;
+
+
+    public Essence(AssetManager assetManager, Vector2 position, Type type) {
+        this.assetManager = assetManager;
+        this.position = position;
+        setType(type);
+    }
+
+    public void create() {
+
+        switch(type) {
+            case GREEN:
+                texture = assetManager.get("Essences/green_essence.png", Texture.class);
+                break;
+
+            case RED:
+                texture = assetManager.get("Essences/red_essence.png", Texture.class);
+                break;
+
+            default:
+                // Something
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+
+        batch.draw(texture, position.x, position.y);
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+}
